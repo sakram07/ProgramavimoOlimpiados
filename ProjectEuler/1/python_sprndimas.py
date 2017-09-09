c5f7a00d4a9224e4a0e634a5c49618a921331ef4
sakram07 <MarkasCerniauskas@gmail.com>
2017-09-09 14:51:44 +0300

    Update python_sprndimas.py

--- ProjectEuler/1/python_sprndimas.py
+++ ProjectEuler/1/python_sprndimas.py
@@ -1,4 +1,4 @@
-#ganėtinai efektyvus spredimo būdas.
-#paprasčiausiai sudeda range sarašus naudojant sum.
-#efektyviau nei paprastas bruteforce
-print(sum(range(0,1000,3))+ sum(range(0,1000,5))-sum(range(0,1000,15)))
+# ganėtinai efektyvus spredimo būdas.
+# paprasčiausiai sudeda range sarašus naudojant sum.
+# efektyviau nei paprastas bruteforce
+print(sum(range(0,1000,3)) + sum(range(0,1000,5)) - sum(range(0,1000,15)))
